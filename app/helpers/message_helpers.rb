8f9e5223441471b44938660446549b1a336ac69b
Mitch Seaman <mitchel.seaman@gmail.com>
2013-07-25 23:25:02 -0700

    fixed a quick bug

--- app/helpers/message_helpers.rb
+++ app/helpers/message_helpers.rb
@@ -17,6 +17,6 @@ end
 
 # account_sid = "ACb29152877bc2516403a6a343d1153ed7"
 # auth_token = "131c02e801c23592d8b34f8f4b004cda"
-client = Twilio::REST::Client.new account_sid, auth_token
+# client = Twilio::REST::Client.new account_sid, auth_token
  
 # from = "+18582076375" # Your Twilio number
