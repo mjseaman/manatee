89a7745dd873eca1eb582850bf235c5766fff044
Mitch Seaman <mitchel.seaman@gmail.com>
2013-07-25 22:05:22 -0700

    revised migrations

---
+++ db/migrate/20130724120647_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email, unique: true
+      t.string :password_hash
+      t.string :first_name
+      t.string :last_name
+      t.string :phone, unique:true
+      t.timestamps
+    end
+
+    add_index :users, :phone, unique: true
+	end
+end
