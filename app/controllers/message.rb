f5cdb2334d1a6c05a7b445072d94d7c439fab20a
Mitch Seaman <mitchel.seaman@gmail.com>
2013-07-30 11:32:40 -0700

    dialed it back

--- app/controllers/message.rb
+++ app/controllers/message.rb
@@ -4,21 +4,21 @@ get '/sms' do
   @sender = User.find_or_create_by_phone(params[:From])
   p "Sender: #{@sender.first_name}"
 
-  @message = Message.create(
-    sid: params[:SmsSid],
-    date_sent: Date.today,
-    from: params[:From],
-    to: params[:To],
-    body: params[:Body]
-    )
+  # @message = Message.create(
+  #   sid: params[:SmsSid],
+  #   date_sent: Date.today,
+  #   from: params[:From],
+  #   to: params[:To],
+  #   body: params[:Body]
+  #   )
 
-  
-  
+  p Response.all.sample.body
+
   sender = params[:From]
   friends = { "+18582139043" => "Mitch" }
   name = friends[sender] || "Mobile Monkey"
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Sms "#{Response.all.sample.body}"
+    r.Sms "Wheeeee!"
   end
   p "*" * 30
   p "The twiml"
